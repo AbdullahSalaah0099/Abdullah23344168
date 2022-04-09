b2242cbc9f16a9ae39c30b6679fcde4192aae46a
Abdullah Salah <95449686+AbdullahSalaah0099@users.noreply.github.com>
2022-04-09 23:57:00 +0200

    Update YOUTUBE DOWNLOADER .py

--- YOUTUBE DOWNLOADER .py
+++ YOUTUBE DOWNLOADER .py
@@ -9,62 +9,63 @@ time.sleep (2)
 os.system ('clear')
 time.sleep (3)
 print ('\033[;092m')
-c=('\033[;092m××××××××××××××××××××××××××××××××××××')
-for I in c+'\n':
-	sys.stdout.write (I)
-	sys.stdout.flush ()
-	time.sleep (00.020)
-print ()
-d =('\033[;092m#####Welcome To AbdullahScript#####')
-for I in d +'\n':
-	sys.stdout.write (I)
-	sys.stdout.flush ()
-	time.sleep (00.10)
-print ()
-p=('\033[;092m##### Script youtube downloader###')
-for I in p+'\n':
-	sys.stdout.write (I)
-	sys.stdout.flush ()
-	time.sleep (00.10)
-print ()
-u=('\033[;092m### Designed By Abdullah Salah###')
-for I in u+'\n':
-	sys.stdout.write (I)
-	sys.stdout.flush ()
-	time.sleep (00.10)
-print ()
-print ('\033[;092m')
+while True:
+    c=('\033[;092m××××××××××××××××××××××××××××××××××××')
+    for I in c+'\n':
+	    sys.stdout.write (I)
+	    sys.stdout.flush ()
+	    time.sleep (00.0020)
+    print ()
+    d =('\033[;092m#####Welcome To AbdullahScript#####')
+    for I in d +'\n':
+	    sys.stdout.write (I)
+	    sys.stdout.flush ()
+	    time.sleep (00.010)
+    print ()
+    p=('\033[;092m##### Script youtube downloader###')
+    for I in p+'\n':
+	    sys.stdout.write (I)
+	    sys.stdout.flush ()
+	    time.sleep (00.010)
+    print ()
+    u=('\033[;092m### Designed By Abdullah Salah###')
+    for I in u+'\n':
+	    sys.stdout.write (I)
+	    sys.stdout.flush ()
+	    time.sleep (00.010)
+    print ()
+    print ('\033[;092m')
 
-i='''\033[;092m Telegram channel : 
+    i='''\033[;092m Telegram channel : 
 
-  ◇ http://t.me/Techno0099
+      ◇ http://t.me/Techno0099
             
 
-Youttube channel  : 
+    Youttube channel  : 
 
- ◇ https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg '''
-for I in i +'\n':
-	sys.stdout.write (I)
-	sys.stdout.flush ()
-	time.sleep (00.030)
-print ()
-h=pyfiglet.figlet_format ('YOUTUBE VID DOWNLOADER')
-for I in h+'\n':
-	sys.stdout.write (I)
-	sys.stdout.flush ()
-	time.sleep (00.0050)
-print ()
-url=input ('Enter The Video Link > :  ')
-print ()
-time.sleep(1)
-print("\033[;093mLoading please wait...")
-t=pytube.YouTube(url).streams.get_highest_resolution().download('/storage/emulated/0')
-if True :
-    time.sleep (3)
+     ◇ https://youtube.com/channel/UCAbtkFAe9yyX0HJNFXyKJUg '''
+    for I in i +'\n':
+	    sys.stdout.write (I)
+	    sys.stdout.flush ()
+	    time.sleep (00.0030)
     print ()
-    print ("\033[;096m")
-    print ('\033[;096mDone Downloading')
-elif False:
-    print("\033[;091mError Try Again")
+    h=pyfiglet.figlet_format ('YOUTUBE VID DOWNLOADER')
+    for I in h+'\n':
+	    sys.stdout.write (I)
+	    sys.stdout.flush ()
+	    time.sleep (00.0050)
+    print ()
+    url=input ('Enter The Video Link > :  ')
+    print ()
+    time.sleep(1)
+    print("\033[;093mLoading please wait...")
+    t=pytube.YouTube(url).streams.get_highest_resolution().download('/storage/emulated/0')
+    if True :
+        time.sleep (3)
+        print ()
+        print ("\033[;096m")
+        print ('\033[;096mDone Downloading')
+    elif False:
+        print("\033[;091mError Try Again")
 
 
